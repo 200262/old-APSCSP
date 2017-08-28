c7eff88fdef722a78c7099d44b9f308ecb0c95f0
200262 <200262@hkis.edu.hk>
2017-08-28 09:15:32 +0800

    Add files via upload

---
+++ Address Book.py
@@ -0,0 +1,55 @@
+
+namelist=[]
+agelist=[]
+numberlist=[]
+
+def inputuser():
+    name=str(input("What is your name?"))
+    name=name.capitalize()
+    age=str(input("What is your age?"))
+    if int(age) > 130:
+        print("Please make sure your age is less than 130.")
+        action()
+    number=str(input("What is your four digit number?"))
+    numberlength=len(number)
+    if numberlength!=4:
+        print("Please make sure your number is four digits.")
+        action()
+    namelist.append(name)
+    agelist.append(age)
+    numberlist.append(number)
+
+def recalluser():
+    lookup=(str(input("Name you're looking up?")))
+    lookup=lookup.capitalize()
+    if lookup in namelist:
+        index=namelist.index(lookup)
+        agefound=agelist[index]
+        numberfound=numberlist[index]
+        print(str(lookup), "is", agefound, "years old. The number we have for them is", numberfound)
+    else:
+        print("User not found")
+
+def action():
+    whichaction=str(input("Press 1 to input, Press 2 to recall"))
+    if whichaction=="1":
+        inputuser()
+        action()
+    elif whichaction=="2":
+        recalluser()
+        action()
+    else:
+        print ("Action not identified.")
+        action()
+
+action()
+
+
+
+
+
+
+
+
+
+
